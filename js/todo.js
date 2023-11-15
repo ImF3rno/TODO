a766719e1ec2c9853657a5e2152a72d04eb8f769
Deltagrandis13 <gabrieliusjac@gmail.com>
2023-11-15 22:36:51 +0200

    Beveik baigtas

--- js/todo.js
+++ js/todo.js
@@ -1,3 +1,6 @@
+window.addEventListener('load', () => {
+    loadTasks();
+});
 function addTodo(){
     const subjectInput=document.querySelector('.subject');
     const subject=subjectInput.value;
@@ -8,8 +11,8 @@ function addTodo(){
         return;
     }
     const newRow=document.createElement('tr');
-    let priorityClass='';
-    switch (priority){
+    let priorityClass = '';
+    switch(priority) {
         case 'low':
             priorityClass='badge bg-success';
             break;
@@ -20,7 +23,7 @@ function addTodo(){
             priorityClass='badge bg-danger';
             break;
     }
-    newRow.innerHTML=`
+    newRow.innerHTML = `
         <td>
             <span class="subjectIcon" onclick="toggleSubjectIcon(this)"><img src="svg/check-mark-black-outline-svgrepo-com.svg" alt="check-mark">${subject}</span>
         </td>
@@ -35,56 +38,68 @@ function addTodo(){
         <td></td>
         <td><img src="svg/remove-round-svgrepo-com.svg" alt="remove" onclick="removeRow(this)"></td>
     `;
-
-    let tasks={
-        subjectName: document.querySelector('.subject').value,
-        priorityName: document.querySelector('.priority').value,
-        dueDateName: document.querySelector('.dueDate').value
-    };
-
-    localStorage.setItem('All-Data', JSON.stringify(tasks))
-    console.log(JSON.parse(localStorage.getItem('All-Data')));
-
+    saveTaskToLocalStorage(subject,priority,dueDate);
     document.getElementById('todoTable').getElementsByTagName('tbody')[0].appendChild(newRow);
     document.querySelector('.subject').value='';
     document.querySelector('.priority').value='low';
     document.querySelector('.dueDate').value='';
 }
-
-// document.querySelector('form').addEventListener('click',(e)=>{
-//     e.preventDefault();
-//     tasks.push({
-//         subjectName:document.querySelector('.subject').value,
-//         priorityName: document.querySelector('.priority').value,
-//         dueDateName: document.querySelector('.dueDate').value
-//     });
-//     localStorage.setItem('tasks',JSON.stringify(tasks))
-// })
-// console.log(JSON.parse(localStorage.getItem('tasks')));
-
 function removeRow(button){
     const row=button.parentNode.parentNode;
+    const subject=row.querySelector('.subjectIcon').innerText;
+    removeTaskFromLocalStorage(subject);
     row.parentNode.removeChild(row);
 }
-
 function toggleSubjectIcon(clickedElement){
     const subjectIcon=clickedElement.querySelector('img');
-    if(subjectIcon.src.includes('svg/check-mark-black-outline-svgrepo-com.svg')){
+    if(subjectIcon.src.includes('svg/check-mark-black-outline-svgrepo-com.svg')) {
         subjectIcon.src='svg/check-circle-svgrepo-com.svg';
     }
 }
-
-
-/** 
- * 
- * ROADMAP:
- * 1. Prideti task'a:
- * - Task'as sukuriamas localStorage
- * 2. Atvaizduojamas task'us:
- * - Task'ai ivedami TODO liste is localStorage
- * 3. Salinti task'a:
- * - Task'ai salinami is localStorage
- * 4. Task' update (prioritetas, progresas):
- * - Atnaujinami duomenys esantys localStorage
- * 
- */
+function saveTaskToLocalStorage(subject,priority,dueDate){
+    const tasks=JSON.parse(localStorage.getItem('tasks')) || [];
+    tasks.push({subject,priority,dueDate});
+    localStorage.setItem('tasks',JSON.stringify(tasks));
+}
+function removeTaskFromLocalStorage(subject) {
+    const tasks=JSON.parse(localStorage.getItem('tasks')) || [];
+    const index=tasks.findIndex(task=>task.subject===subject);
+    if (index!==-1){
+        tasks.splice(index,1);
+    }
+    localStorage.setItem('tasks',JSON.stringify(tasks));
+}
+function loadTasks(){
+    const tasks=JSON.parse(localStorage.getItem('tasks')) || [];
+    tasks.forEach(task=>{
+        const newRow=document.createElement('tr');
+        let priorityClass='';
+        switch (task.priority){
+            case 'low':
+                priorityClass='badge bg-success';
+                break;
+            case 'medium':
+                priorityClass='badge bg-info';
+                break;
+            case 'high':
+                priorityClass='badge bg-danger';
+                break;
+        }
+        newRow.innerHTML = `
+            <td>
+                <span class="subjectIcon" onclick="toggleSubjectIcon(this)"><img src="svg/check-mark-black-outline-svgrepo-com.svg" alt="check-mark">${task.subject}</span>
+            </td>
+            <td class="${priorityClass}">${task.priority}</td>
+            <td>${task.dueDate}</td>
+            <td>New</td>
+            <td>
+                <div class="progress">
+                    <div id="progress-bar" class="progress-bar bg-success" role="progressbar" style="width:0%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </td>
+            <td></td>
+            <td><img src="svg/remove-round-svgrepo-com.svg" alt="remove" onclick="removeRow(this)"></td>
+        `;
+        document.getElementById('todoTable').getElementsByTagName('tbody')[0].appendChild(newRow);
+    });
+}
